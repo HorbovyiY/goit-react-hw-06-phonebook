1ef955699efee487c64345004a9e416d95e97f15
HorbovyiY <yhorbovyi@gmail.com>
2023-04-21 06:55:59 +0300

    додає збереження в локальне сховище

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,9 @@
-import {useState, useEffect} from "react";
-
 import { Form } from "./Form/Form";
 import { Contacts } from "./Contacts/Contacts";
 import { Filter } from "./Filter/Filter";
 import { Title } from "./App.styled";
 
 export const App = () => {
-  const [contacts, setContacts] = useState([]);
-
-  useEffect(() => { 
-    const contactsBook = JSON.parse(localStorage.getItem("contacts"));
-    if (contactsBook) {setContacts(contactsBook)}
-  }, [])
-  
-  useEffect(() => { 
-    localStorage.setItem("contacts", JSON.stringify(contacts))
-  },[contacts])
-
 return(
     <div
       style={{
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
 import contactsReducer from './contactsSlice'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-export default configureStore({
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedcontactsReducer = persistReducer(persistConfig, contactsReducer);
+
+export const store = configureStore({
     reducer: {
-        contacts: contactsReducer,
+        contacts: persistedcontactsReducer,
     },
 })
+
+export const persistor = persistStore(store)
